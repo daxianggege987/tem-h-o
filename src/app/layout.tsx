a097ea5daf3a26a30754034884a5302b379bfcfb
王大锤 <g94722424@gmail.com>
2025-07-07 05:16:29 +0000

    好的，我想在源代码里面加上一段统计代码，用来统计访客数据的，如果想要在所有的页面都加上这一段代码，需要怎么做？ 提供统计代码的第三方平台给出了3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/context/AuthContext'; // Import AuthProvider
@@ -20,6 +20,11 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet" />
+        <Script
+          id="wukong-analytics"
+          strategy="beforeInteractive"
+          src="//api.wukongtongji.com/c?_=800710351182897152"
+        />
       </head>
       <body className="font-body antialiased">
         <AuthProvider> {/* Wrap children with AuthProvider */}
